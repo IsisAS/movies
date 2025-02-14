a58488e5e6db89132d704f752a9d5e230fae2b91
IsisAS <isis.alvesilva@gmail.com>
2025-02-13 21:51:47 -0300

    feat: enhance popular movies display with title and overview overlay

--- src/components/popular-movies/index.tsx
+++ src/components/popular-movies/index.tsx
@@ -31,12 +31,16 @@ const PopularMovies: React.FC = () => {
                     >
                         {movies.map((movie) => (
                             <SwiperSlide key={movie.id} className="flex justify-center">
-                                <div className="w-full transform transition duration-300 hover:scale-105">
+                                <div className="w-full flex items-center justify-center transform transition duration-300 hover:scale-105">
                                     <img
                                         src={`https://image.tmdb.org/t/p/w780${movie.backdrop_path}`}
                                         alt={movie.title}
                                         className="rounded-lg shadow-lg w-full h-[60vh] object-cover"
                                     />
+                                    <div className="absolute bottom-0 left-0 w-full pl-10 pb-20 bg-gradient-to-t from-black to-transparent">
+                                        <h1 className="text-white text-xl font-bold">{movie.title}</h1>
+                                        <p className="text-gray-400 text-sm w-2/4">{movie.overview}</p>
+                                    </div>
                                 </div>
                             </SwiperSlide>
                         ))}
